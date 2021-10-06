cf2eb015aa5c8a72fbfca05edc4501bf7dc59921
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-06 11:21:14 +0200

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,24 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>About Us | Hello City</title>
-</head>
-
-<body>
+@section('content')
 
     <p>Built with &hearts; by Jimmy MARTIN</p>
 
     <p><a href="/">Revenenir à la page d'accueil</a></p>
-
-    <footer>
-        <p>
-            &copy; Copyright {{ date('Y') }}
-        </p>
-    </footer>
-</body>
-
-</html>
+    
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,26 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
 
-    <title>Hello City</title>
-</head>
-
-<body>
     <h1>Hello from Quebec !</h1>
 
     <p>It's currently {{ date('h:i A') }}.</p>
 
-    <footer>
-        <p>
-            &copy; Copyright {{ date('Y') }} &middot;
-            <a href="/about-us">
-                About US
-            </a>
-        </p>
-    </footer>
-</body>
-
-</html>
+@endsection
